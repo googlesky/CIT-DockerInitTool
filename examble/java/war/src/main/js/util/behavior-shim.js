a600c75e7c797ae87c3140956832be583a48d853
Hieu Le <lphieu@tma.com.vn>
2018-12-07 22:12:01 +0700

    Add Java test

--- examble/java/war/src/main/js/util/behavior-shim.js
+++
@@ -1,4 +0,0 @@
-
-exports.specify = function(selector, id, priority, behavior) {
-    Behaviour.specify(selector, id, priority, behavior); // jshint ignore:line
-};
--- examble/java/war/src/main/js/widgets/config/model/util.js
+++
@@ -1,5 +0,0 @@
-
-exports.toId = function(string) {
-    string = string.trim();
-    return 'config_' + string.replace(/[\W_]+/g, '_').toLowerCase();
-};
